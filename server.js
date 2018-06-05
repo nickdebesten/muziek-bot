18ca039dab446ab074aa976435f6d38efb483559
nickdebesten <36811561+nickdebesten@users.noreply.github.com>
2018-06-05 09:07:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -94,4 +94,4 @@ client.on('message', msg => {
 	if (!msg.content.startsWith(tokens.prefix)) return;
 	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](msg);
 });
-client.login(tokens.d_token);
+client.login(process.env.TOKEN);
